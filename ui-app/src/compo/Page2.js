dbe54cc87b494ff6eed7b9501b9f02a246a0dac2
Shivang Purrohit <104918730+Shivang-purrohit@users.noreply.github.com>
2022-07-11 15:34:31 +0530

    updated

--- ui-app/src/compo/Page2.js
+++ ui-app/src/compo/Page2.js
@@ -1,15 +1,29 @@
 import React from 'react'
-
+import './Page2.css'
+import img from './images/img2.svg'
 export default function Page2() {
   return (
  <>
-    <div className=''>
-        <div className='nav-section'>
+    <div>
+    <nav>
+
+    
+    
+    
             
-
-        </div>
-
-      
+            <ul className='ul1'>
+            <h2 className='h11'>MATRIX-UI
+    <span> <img className='image2' src={img} alt="" /></span></h2>
+           
+            
+                <li className='list'>NavBar </li>
+                <li className='list'>Cards</li>
+                <li className='list'>Buttons </li>
+                <li className='list'>floating buttons </li>
+                <li className='list'>Alerts </li>
+            </ul>
+          
+            </nav>
       </div>
   </>
   )
