d29ef7b81d192ae0806d305e8240c15da4a5a100
Shivang Purrohit <104918730+Shivang-purrohit@users.noreply.github.com>
2022-07-12 14:31:44 +0530

    done with navbar

--- ui-app/src/compo/Navbar.js
+++ ui-app/src/compo/Navbar.js
@@ -1,26 +1,43 @@
 import React from 'react'
 import img from './images/img2.svg';
-const navbar = () => {
+import './components.css';
+
+
+export default function Navbar() {
   return (
-   <>
-   
-    <div className='container'>
-      <navbar className='top'>
-     <div> <p>MATRIX-UI</p></div> 
-     <img className='image-logo' src={img} alt="" />
-      
-      <div className='right'>
-      <a href="https://github.com/Shivang-purrohit">
-      <i className='fa-brands fa-github '></i>
-      </a>
-       </div>
-      </navbar>
+    <>
+    <nav className="navbar">
+     <div className='nav-logo'>MATRIX-UI</div>
+       <ul>
+         <li className='inline'> <a href='/'  className="navItem">MEN</a></li>
+         <li className='inline'> <a href='/'  className="navItem">WOMEN</a></li>
+         <li className='inline'> <a href='/'  className="navItem">KIDS</a></li>
+         <li className='inline'> <a href='/'  className="navItem">ELECTRONICS</a></li>
+         <li className='inline'><input className='search' placeholder='Search'/></li>
+       </ul>
+       
+     
+ 
+ 
+    </nav>
 
+<br />
 
-    </div>
-
-   </>
+    <nav className="navbar dark">
+     <div className='nav-logo'>MATRIX-UI</div>
+       <ul>
+         <li className='inline '> <a href='/'  className="navItem dark">MEN</a></li>
+         <li className='inline'> <a href='/'  className="navItem dark">WOMEN</a></li>
+         <li className='inline'> <a href='/'  className="navItem dark">KIDS</a></li>
+         <li className='inline'> <a href='/'  className="navItem dark">ELECTRONICS</a></li>
+         <li className='inline'><input className='search' placeholder='Search'/></li>
+       </ul>
+       
+     
+ 
+ 
+    </nav>
+     
+    </>
   )
 }
-
-export default navbar
--- ui-app/src/compo/Page2.js
+++ ui-app/src/compo/Page2.js
@@ -1,30 +1,120 @@
 import React from 'react'
 import './Page2.css'
 import img from './images/img2.svg'
+import { Link } from 'react-router-dom'
+import Navbar from './Navbar'
 export default function Page2() {
   return (
  <>
-    <div>
-    <nav>
-
-    
-    
-    
-            
+ 
+    <div className='wrapper'>
+      <div className="sidebar">
+     
+      <h2> <span> <img className='page2-img' src={img} alt="" /></span> MATRIX-UI </h2>
             <ul className='ul1'>
-            <h2 className='h11'>MATRIX-UI
-    <span> <img className='image2' src={img} alt="" /></span></h2>
+            
+    
            
             
-                <li className='list'>NavBar </li>
-                <li className='list'>Cards</li>
-                <li className='list'>Buttons </li>
-                <li className='list'>floating buttons </li>
-                <li className='list'>Alerts </li>
+                <li  className='list' ><Link to="/" smooth={true} duration={1000}  className='links'></Link><i class="fa-solid fa-bars"></i> NavBar </li>
+                <li  className='list' ><Link to="/"  className='links'></Link><i class="fa-solid fa-square-full"></i> Cards </li>
+                <li  className='list' ><Link to="/"  className='links'></Link><i class="fa-solid fa-stop"></i> Buttons </li>
+                <li  className='list' ><Link to="/"  className='links'></Link> <i class="fa-solid fa-circle-dot"></i> Floating Buttons</li>
+                <li  className='list' ><Link to="/"  className='links'></Link> <i class="fa-solid fa-triangle-exclamation"></i> Alerts </li>
+                <li  className='list' ><Link to="/"  className='links'></Link> <i class="fa-solid fa-id-badge"></i>  Badge on Card</li>
+                <li  className='list' ><Link to="/"  className='links'></Link><i class="fa-solid fa-user-tie"></i> Avatar</li>
             </ul>
-          
-            </nav>
+
+             
+            </div>
       </div>
+
+
+            <div className="main-content">
+            <h1>INSTALLATION</h1>
+            <p className='para'>copy this code in the head tag of your HTML document and you are good to go:</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim labore earum error corporis ratione deleniti aspernatur, illo reiciendis eveniet aliquid ea voluptatibus blanditiis accusamus atque quae! Repellendus porro consequuntur optio cupiditate sit inventore impedit vero? Soluta inventore maxime est. Quam ad minus veritatis iusto earum doloremque asperiores nesciunt reiciendis consequuntur explicabo, fuga nostrum quae tempora accusamus totam? Perferendis corporis aperiam molestiae ratione autem inventore voluptatem, dolorum reprehenderit ea sed culpa cumque, voluptates porro esse placeat amet excepturi! Molestiae libero, dolore enim repudiandae neque obcaecati quam eos illo autem, assumenda delectus. Non deleniti dolorum blanditiis laudantium distinctio suscipit voluptatibus doloremque ipsum doloribus eos. Voluptates, facere placeat a ab doloribus facilis, molestias id nostrum esse veniam officia tempora hic enim numquam beatae magnam sint dolorum eum consequuntur omnis voluptate! Eveniet magni deleniti quasi modi quos aliquid odio, magnam corrupti error, ipsam architecto sunt. Nemo inventore voluptas corporis eum doloremque quidem aut? Eligendi ipsum numquam incidunt, a nisi accusantium facere quasi? Repellendus ab voluptatem, excepturi aliquid et ipsam quis hic maxime quasi qui nobis non officiis at, corporis exercitationem possimus deleniti iste sint perferendis. Laudantium natus nihil at iste voluptate voluptatem ullam veritatis, rerum aspernatur eius aliquam labore, laborum exercitationem officiis accusamus amet similique nulla. Dolor sapiente aliquam saepe quaerat! Beatae deleniti perspiciatis veniam quis facere dolore, velit .</p>
+            <hr></hr>
+
+             <br></br>
+            <div  id='NavBar'>
+              <h1>NavBar</h1>
+              <br />
+              <Navbar/>
+              <br />
+              <br />
+              
+              <br />
+              <br />
+              <br />
+              <br />
+
+              
+            </div>
+            <hr />
+            <br />
+            
+            <h1>Cards</h1>
+            <p className='para'>copy this code in the head tag of your HTML document and you are good to go:</p>
+            <p className='para'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim labore earum error corporis ratione deleniti aspernatur, illo reiciendis eveniet aliquid ea voluptatibus blanditiis accusamus atque quae! Repellendus porro consequuntur optio cupiditate sit inventore impedit vero? Soluta inventore maxime est. Quam ad minus veritatis iusto earum doloremque asperiores nesciunt reiciendis consequuntur explicabo, fuga nostrum quae tempora accusamus totam? Perferendis corporis aperiam molestiae ratione autem inventore voluptatem, dolorum reprehenderit ea sed culpa cumque, voluptates porro esse placeat amet excepturi! Molestiae libero, dolore enim repudiandae neque obcaecati quam eos illo autem, assumenda delectus. Non deleniti dolorum blanditiis laudantium distinctio suscipit voluptatibus doloremque ipsum doloribus eos. Voluptates, facere placeat a ab doloribus facilis, molestias id nostrum esse veniam officia tempora hic enim numquam beatae magnam sint dolorum eum consequuntur omnis voluptate! Eveniet magni deleniti quasi modi quos aliquid odio, magnam corrupti error, ipsam architecto sunt. Nemo inventore voluptas corporis eum doloremque quidem aut? Eligendi ipsum numquam incidunt, a nisi accusantium facere quasi? Repellendus ab voluptatem, excepturi aliquid et ipsam quis hic maxime quasi qui nobis non officiis at, corporis exercitationem possimus deleniti iste sint perferendis. Laudantium natus nihil at iste voluptate voluptatem ullam veritatis, rerum aspernatur eius aliquam labore, laborum exercitationem officiis accusamus amet similique nulla. Dolor sapiente aliquam saepe quaerat! Beatae deleniti perspiciatis veniam quis facere dolore, velit .</p>
+            <hr></hr>
+
+             <br></br>
+
+
+             <h1>Buttons</h1>
+           
+            <p className='para'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim labore earum error corporis ratione deleniti aspernatur, illo reiciendis eveniet aliquid ea voluptatibus blanditiis accusamus atque quae! Repellendus porro consequuntur optio cupiditate sit inventore impedit vero? Soluta inventore maxime est. Quam ad minus veritatis iusto earum doloremque asperiores nesciunt reiciendis consequuntur explicabo, fuga nostrum quae tempora accusamus totam? Perferendis corporis aperiam molestiae ratione autem inventore voluptatem, dolorum reprehenderit ea sed culpa cumque, voluptates porro esse placeat amet excepturi! Molestiae libero, dolore enim repudiandae neque obcaecati quam eos illo autem, assumenda delectus. Non deleniti dolorum blanditiis laudantium distinctio suscipit voluptatibus doloremque ipsum doloribus eos. Voluptates, saepe quaerat! Beatae deleniti perspiciatis veniam quis facere dolore, velit .</p>
+            <hr></hr>
+
+             <br></br>
+
+             <h1>Floating Button</h1>
+            
+            <p className='para'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim labore earum error corporis ratione deleniti aspernatur, illo reiciendis eveniet aliquid ea voluptatibus blanditiis accusamus atque quae! Repellendus porro consequuntur optio cupiditate sit inventore impedit vero? Soluta inventore maxime est. Quam ad minus veritatis iusto earum doloremque asperiores nesciunt reiciendis consequuntur explicabo, fuga nostrum quae tempora accusamus totam? Perferendis corporis aperiam molestiae ratione autem inventore voluptatem, dolorum reprehenderit ea sed culpa cumque, voluptates porro esse placeat amet excepturi! Molestiae libero, dolore enim repudiandae neque obcaecati quam eos illo autem, assumenda delectus. Non deleniti dolorum blanditiis laudantium distinctio suscipit voluptatibus doloremque ipsum doloribus eos. Voluptates, saepe quaerat! Beatae deleniti perspiciatis veniam quis facere dolore, velit .</p>
+            <hr></hr>
+
+             <br></br>
+
+             <h1>Alerts</h1>
+           
+            <p className='para'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim labore earum error corporis ratione deleniti aspernatur, illo reiciendis eveniet aliquid ea voluptatibus blanditiis accusamus atque quae! Repellendus porro consequuntur optio cupiditate sit inventore impedit vero? Soluta inventore maxime est. Quam ad minus veritatis iusto earum doloremque asperiores nesciunt reiciendis consequuntur explicabo, fuga nostrum quae tempora accusamus totam? Perferendis corporis aperiam molestiae ratione autem inventore voluptatem, dolorum reprehenderit ea sed culpa cumque, voluptates porro esse placeat amet excepturi! Molestiae libero, dolore enim repudiandae neque obcaecati quam eos illo autem, assumenda delectus. Non deleniti dolorum blanditiis laudantium distinctio suscipit voluptatibus doloremque ipsum doloribus eos. Voluptates, saepe quaerat! Beatae deleniti perspiciatis veniam quis facere dolore, velit .</p>
+            <hr></hr>
+
+             <br></br>
+
+
+
+              
+             <h1>Badge on cards</h1>
+            
+            <p className='para'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim labore earum error corporis ratione deleniti aspernatur, illo reiciendis eveniet aliquid ea voluptatibus blanditiis accusamus atque quae! Repellendus porro consequuntur optio cupiditate sit inventore impedit vero? Soluta inventore maxime est. Quam ad minus veritatis iusto earum doloremque asperiores nesciunt reiciendis consequuntur explicabo, fuga nostrum quae tempora accusamus totam? Perferendis corporis aperiam molestiae ratione autem inventore voluptatem, dolorum reprehenderit ea sed culpa cumque, voluptates porro esse placeat amet excepturi! Molestiae libero, dolore enim repudiandae neque obcaecati quam eos illo autem, assumenda delectus. Non deleniti dolorum blanditiis laudantium distinctio suscipit voluptatibus doloremque ipsum doloribus eos. Voluptates, saepe quaerat! Beatae deleniti perspiciatis veniam quis facere dolore, velit .</p>
+            <hr></hr>
+
+             <br></br>
+           
+           
+             <h1>Avatar</h1>
+          
+            <p className='para'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim labore earum error corporis ratione deleniti aspernatur, illo reiciendis eveniet aliquid ea voluptatibus blanditiis accusamus atque quae! Repellendus porro consequuntur optio cupiditate sit inventore impedit vero? Soluta inventore maxime est. Quam ad minus veritatis iusto earum doloremque asperiores nesciunt reiciendis consequuntur explicabo, fuga nostrum quae tempora accusamus totam? Perferendis corporis aperiam molestiae ratione autem inventore voluptatem, dolorum reprehenderit ea sed culpa cumque, voluptates porro esse placeat amet excepturi! Molestiae libero, dolore enim repudiandae neque obcaecati quam eos illo autem, assumenda delectus. Non deleniti dolorum blanditiis laudantium distinctio suscipit voluptatibus doloremque ipsum doloribus eos. Voluptates, saepe quaerat! Beatae deleniti perspiciatis veniam quis facere dolore, velit .</p>
+            <hr></hr>
+
+             <br></br>
+           
+            
+           
+           
+           
+           
+            </div>
+
+            
+           
   </>
   )
 }
+
+
+
+
+
