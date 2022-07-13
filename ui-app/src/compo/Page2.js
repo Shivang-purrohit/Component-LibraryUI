b03c17e296153c8e2c724375cd5fcb592de5da71
Shivang Purrohit <104918730+Shivang-purrohit@users.noreply.github.com>
2022-07-13 16:41:46 +0530

    updated with cards

--- ui-app/src/compo/Page2.js
+++ ui-app/src/compo/Page2.js
@@ -3,6 +3,7 @@ import './Page2.css'
 import img from './images/img2.svg'
 import { Link } from 'react-router-dom'
 import Navbar from './Navbar'
+import Cards from './Cards'
 export default function Page2() {
   return (
  <>
@@ -56,7 +57,15 @@ export default function Page2() {
             
             <h1>Cards</h1>
             <p className='para'>copy this code in the head tag of your HTML document and you are good to go:</p>
-            <p className='para'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim labore earum error corporis ratione deleniti aspernatur, illo reiciendis eveniet aliquid ea voluptatibus blanditiis accusamus atque quae! Repellendus porro consequuntur optio cupiditate sit inventore impedit vero? Soluta inventore maxime est. Quam ad minus veritatis iusto earum doloremque asperiores nesciunt reiciendis consequuntur explicabo, fuga nostrum quae tempora accusamus totam? Perferendis corporis aperiam molestiae ratione autem inventore voluptatem, dolorum reprehenderit ea sed culpa cumque, voluptates porro esse placeat amet excepturi! Molestiae libero, dolore enim repudiandae neque obcaecati quam eos illo autem, assumenda delectus. Non deleniti dolorum blanditiis laudantium distinctio suscipit voluptatibus doloremque ipsum doloribus eos. Voluptates, facere placeat a ab doloribus facilis, molestias id nostrum esse veniam officia tempora hic enim numquam beatae magnam sint dolorum eum consequuntur omnis voluptate! Eveniet magni deleniti quasi modi quos aliquid odio, magnam corrupti error, ipsam architecto sunt. Nemo inventore voluptas corporis eum doloremque quidem aut? Eligendi ipsum numquam incidunt, a nisi accusantium facere quasi? Repellendus ab voluptatem, excepturi aliquid et ipsam quis hic maxime quasi qui nobis non officiis at, corporis exercitationem possimus deleniti iste sint perferendis. Laudantium natus nihil at iste voluptate voluptatem ullam veritatis, rerum aspernatur eius aliquam labore, laborum exercitationem officiis accusamus amet similique nulla. Dolor sapiente aliquam saepe quaerat! Beatae deleniti perspiciatis veniam quis facere dolore, velit .</p>
+            <Cards/>
+            <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+
             <hr></hr>
 
              <br></br>
