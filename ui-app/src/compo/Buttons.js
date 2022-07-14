ba11e574a405b52214be4c59ba7ba8ab37a890e3
Shivang Purrohit <104918730+Shivang-purrohit@users.noreply.github.com>
2022-07-14 13:56:53 +0530

    updated

---
+++ ui-app/src/compo/Buttons.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Buttons() {
+  return (
+    <>
+    <div className='buttons'>
+      
+        <a href='' className="button1 design1 des-set1">primary</a>
+        <a href='' className="button1 design2 des-set2">Danger</a>
+        <a href='' className="button1 design3 des-set3">Warning</a>
+        <a href='' className="button1 design4 des-set4">Success</a>
+      
+
+
+      
+    </div>
+    </>
+  )
+}
