9679d42dafe9601ea6578d143325b8521b861381
Shivang Purrohit <104918730+Shivang-purrohit@users.noreply.github.com>
2022-07-16 10:01:09 +0530

    updated

--- ui-app/src/compo/Floating.js
+++ ui-app/src/compo/Floating.js
@@ -8,7 +8,7 @@ export default function Floating() {
             
                   <a href="" className="float-btn float1">+</a> 
                   <a href="" className="float-btn float2">+</a>
-                  <a href="" className="float-btn float2">+</a> 
+                  <a href="" className="float-btn float3">+</a> 
                   <a href="" className="float-btn float4">+</a>
            
         </div>
